676d1ee8602cd3bd4293d377bcc909f88ba5202b
Ameesha Singh <ameesha.singh@bounteous.com>
2025-08-29 19:21:39 +0530

    finallllchanges

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,30 +1,3 @@
-// import { fetchProduct } from "@/lib/api";
-// import AddToCartButton from "./AddToCartButton";
-// import Image from "next/image";
-
-// export default async function ProductDetailPage({ params }: { params: { id: string } }) {
-//   const product = await fetchProduct(params.id);
-//   return (
-//     <div className="grid md:grid-cols-2 gap-12">
-//      <Image
-//   src={product.image}      // your image URL
-//   alt={product.title}      // alt text for accessibility
-//   width={400}              // specify width
-//   height={400}             // specify height
-//   className="w-full h-96 object-contain"
-//   priority={true}          // optional: loads this image eagerly (good for hero/product images)
-// />
-//       <div>
-//         <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
-//         <p className="text-gray-600 mb-6">{product.description}</p>
-//         <p className="text-2xl font-bold text-blue-600 mb-6">${product.price}</p>
-//         <AddToCartButton product={product} />
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import { fetchProduct } from "@/lib/api";
 import AddToCartButton from "./AddToCartButton";
 import Image from "next/image";
@@ -37,8 +10,14 @@ interface Product {
   image: string;
 }
 
-export default async function ProductDetailPage({ params }: any) {
-  const product: Product = await fetchProduct(params.id);
+// Async server component returning Promise<JSX.Element>
+export default async function ProductDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const {id} = await params;
+  const product: Product = await fetchProduct(id);
 
   return (
     <div className="grid md:grid-cols-2 gap-12">
